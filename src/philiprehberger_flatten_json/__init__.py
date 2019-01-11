17ed878926576bc9c253d0f5c36d25967ff47c2e
Reverend Philip <me@philiprehberger.com>
2019-01-10 20:22:48 -0600

    Initial release v0.1.0

---
+++ src/philiprehberger_flatten_json/__init__.py
@@ -0,0 +1,119 @@
+"""Flatten nested JSON/dicts into dot-notation keys and unflatten back."""
+
+from __future__ import annotations
+
+from typing import Any
+
+
+__all__ = [
+    "flatten",
+    "unflatten",
+]
+
+
+def flatten(
+    data: dict | list,
+    *,
+    separator: str = ".",
+    max_depth: int = 0,
+) -> dict[str, Any]:
+    """Flatten a nested dict/list into a flat dict with dot-notation keys.
+
+    Args:
+        data: Nested dict or list.
+        separator: Key separator. Defaults to ``"."``.
+        max_depth: Maximum nesting depth to flatten. 0 means unlimited.
+
+    Returns:
+        Flat dict with composite keys.
+    """
+    result: dict[str, Any] = {}
+    _flatten_recursive(data, "", separator, max_depth, 0, result)
+    return result
+
+
+def _flatten_recursive(
+    data: Any,
+    prefix: str,
+    separator: str,
+    max_depth: int,
+    current_depth: int,
+    result: dict[str, Any],
+) -> None:
+    if max_depth > 0 and current_depth >= max_depth:
+        result[prefix] = data
+        return
+
+    if isinstance(data, dict):
+        if not data:
+            if prefix:
+                result[prefix] = data
+            return
+        for key, value in data.items():
+            new_key = f"{prefix}{separator}{key}" if prefix else key
+            _flatten_recursive(value, new_key, separator, max_depth, current_depth + 1, result)
+    elif isinstance(data, list):
+        if not data:
+            if prefix:
+                result[prefix] = data
+            return
+        for i, value in enumerate(data):
+            new_key = f"{prefix}{separator}{i}" if prefix else str(i)
+            _flatten_recursive(value, new_key, separator, max_depth, current_depth + 1, result)
+    else:
+        result[prefix] = data
+
+
+def unflatten(data: dict[str, Any], *, separator: str = ".") -> dict | list:
+    """Unflatten a flat dict with dot-notation keys back to nested structure.
+
+    Args:
+        data: Flat dict with composite keys.
+        separator: Key separator used during flattening.
+
+    Returns:
+        Nested dict or list.
+    """
+    if not data:
+        return {}
+
+    result: dict[str, Any] = {}
+
+    for compound_key, value in data.items():
+        keys = compound_key.split(separator)
+        _set_nested(result, keys, value)
+
+    return _convert_numeric_dicts(result)
+
+
+def _set_nested(container: dict, keys: list[str], value: Any) -> None:
+    for key in keys[:-1]:
+        if key not in container:
+            container[key] = {}
+        container = container[key]
+    container[keys[-1]] = value
+
+
+def _convert_numeric_dicts(data: Any) -> Any:
+    """Recursively convert dicts with all-numeric keys to lists."""
+    if not isinstance(data, dict):
+        return data
+
+    # Check if all keys are numeric
+    if data and all(_is_int(k) for k in data):
+        max_idx = max(int(k) for k in data)
+        if max_idx < len(data) * 2:  # reasonable density check
+            lst = [None] * (max_idx + 1)
+            for k, v in data.items():
+                lst[int(k)] = _convert_numeric_dicts(v)
+            return lst
+
+    return {k: _convert_numeric_dicts(v) for k, v in data.items()}
+
+
+def _is_int(s: str) -> bool:
+    try:
+        int(s)
+    except ValueError:
+        return False
+    return True
